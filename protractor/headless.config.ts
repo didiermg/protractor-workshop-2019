9e8de7ddcc45976a867081f0db43dc54498663a5
didiermg <damartinez160@gmail.com>
2019-09-30 11:23:08 -0500

    css selector (#9)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,9 +3,10 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/**/*.spec.js'],
   seleniumAddress: 'http://localhost:4444/wd/hub',
   SELENIUM_PROMISE_MANAGER: false,
+  getPageTimeout: 1000,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
